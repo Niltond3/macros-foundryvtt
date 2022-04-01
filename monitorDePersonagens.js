611c9a990f8da97c3839fc308b8443489b676759
Nilton Ferreira Diniz Neto <nilton@pop-os.localdomain>
2022-04-01 09:08:36 -0300

    feat: substitute literal svg to boostrap icons

--- monitorDePersonagens.js
+++ monitorDePersonagens.js
@@ -71,36 +71,7 @@ async function main() {
     RouseReRoll: game.i18n.localize("VTM5E.RouseReRoll"),
     BaneSeverity: game.i18n.localize("VTM5E.BaneSeverity"),
   };
-  let getSvg = {
-    drop: `
-          <svg width="25px" viewbox="0 0 50 42">
-            <path class="svg-drop"
-              d="M15 6
-              Q 15 6, 25 18
-              A 12.8 12.8 0 1 1 5 18
-              Q 15 6 15 6z"/>
-          </svg>`,
-    network: `
-            <svg width="20" height="20" viewBox="61.239 82.669 276.924 236.515">
-              <path 
-                d="M138.163 83.078
-                c-11.854 1.982-21.682 8.771-29.076 20.086-17.375 26.59-12.092 64.338 11.56 82.596 5.123 3.955 5.259 4.121 5.163 6.316-.236 5.425-3.184 7.934-18.667 15.885-18.682 9.594-23.108 12.474-29.619 19.273-9.4 9.818-14.36 21.394-15.876 37.052-.24 2.469-.527 5.178-.638 6.02l-.203 1.531h57.896l5.036-5.017c8.38-8.348 17.086-14.261 32.36-21.977 4.161-2.102 7.568-3.945 7.57-4.094.003-.15-2.19-3.229-4.873-6.843-10.002-13.477-13.986-23.295-15.563-38.362-3.459-33.034 10.222-60.656 36.563-73.821 4.823-2.411 5.031-2.727 3.881-5.9-7.87-21.717-27.405-35.771-45.514-32.745m111.664.198c-12.978 2.722-24.584 12.964-31.731 27.999-3.678 7.739-3.689 7.707 3.974 11.614 38.758 19.76 47.371 77.266 16.976 113.349-1.945 2.309-3.536 4.282-3.536 4.386 0 .104 3.995 2.19 8.878 4.635 16.471 8.251 23.205 12.85 32.113 21.933l4.561 4.65 28.551-.105 28.55-.104-.11-5.295c-.256-12.306-4.495-25.067-11.371-34.229-6.374-8.493-13.568-13.616-33.553-23.897-17.141-8.818-17.956-9.504-18.689-15.738l-.268-2.279 1.996-1.718c13.723-11.812 20.37-22.03 23.387-35.952 6.466-29.845-8.522-60.997-32.845-68.268-4.023-1.203-13.265-1.74-16.883-.981m-55.133 47.747c-10.132 1.005-21.43 8.503-28.393 18.842-18.475 27.432-12.334 67.914 12.905 85.066l3.039 2.065-.004 2.624c-.011 6.296-.06 6.334-25.506 19.76-21.947 11.579-31.456 22.086-36.074 39.86-1.418 5.459-3.029 19.072-2.338 19.763.099.099 36.976.181 81.949.181h81.769l-.004-.919c-.035-8.242-3.074-21.744-6.545-29.081-6.52-13.783-15.269-21.238-38.961-33.198-16.802-8.481-17.959-9.449-18.78-15.707-.331-2.518.139-3.241 3.997-6.144 26.728-20.116 30.222-64.981 7.007-89.97-6.603-7.107-15.712-12.384-22.298-12.916-5.624-.455-8.836-.517-11.763-.226" fill-rule="evenodd"/>
-            </svg>`,
-    person: `
-              <svg class="image ts-convic-image ts-convic-svg" viewBox="0 0 512 512" style="height: 50px; width: 50px;">
-                <path d="M0 0h512v512H0z" fill="#000" fill-opacity="1"></path>
-                <g class="" transform="translate(0,0)" style="">
-                  <path 
-                    d="M250.882 22.802c-23.366 3.035-44.553 30.444-44.553 65.935 0 19.558 6.771 36.856 16.695 48.815l11.84 14.263-18.217 3.424
-                    c-12.9 2.425-22.358 9.24-30.443 20.336-8.085 11.097-14.266 26.558-18.598 44.375-7.843 32.28-9.568 71.693-9.842 106.436
-                    h42.868l11.771 157.836c29.894 6.748 61.811 6.51 90.602.025l10.414-157.86h40.816
-                    c-.027-35.169-.477-75.126-7.584-107.65-3.918-17.934-9.858-33.372-18.04-44.343-8.185-10.97-18.08-17.745-32.563-19.989
-                    l-18.592-2.88 11.736-14.704c9.495-11.897 15.932-28.997 15.932-48.082 0-37.838-23.655-65.844-49.399-65.844z" 
-                    fill="#fff" fill-opacity="1">
-                  </path>
-                </g>
-              </svg>`,
-  };
+  
   function getBloodPotencyText(level) {
     const BLOOD_POTENCY_TEXT = [
       {
@@ -243,7 +214,7 @@ async function main() {
     let getTooltip = {
       bloodPotency: `
       <div class="tooltip blood-potency" data="${bloodPotencyValue}">
-          ${getSvg.drop}
+          <i class="bi bi-droplet-fill bi-tooltip"></i>
           <div class="tooltip-body blood-potency-body">
               <div class="label-wrapper">
                   <span class="label-resources blood-gifts-label">${getPlaceholder.bloodSurge}</span>
@@ -273,7 +244,7 @@ async function main() {
       </div>`,
       touchstones: `
       <div class="tooltip touchstones">
-          ${getSvg.network}
+          <i class="bi bi-people-fill bi-tooltip"></i>
           <div class="tooltip-body touchstones-body">
               ${touchstonesList
                 .map(
@@ -282,7 +253,7 @@ async function main() {
                   ${
                     tsConvic.touchstoneImage != "img"
                       ? `<img class="image ts-convic-image" src=${tsConvic.touchstoneImage}/>`
-                      : getSvg.person
+                      : `<i class="bi bi-person-bounding-box"></i>`
                   }
                   <span class="label-resources blood-gifts-label ts-name">${
                     tsConvic.touchstone
@@ -295,6 +266,9 @@ async function main() {
                 .join("")}
           </div>
       </div>`,
+      attributes: `
+      
+      `,
     };
 
     headerWrapper.innerHTML += `
@@ -396,19 +370,20 @@ async function main() {
   .tooltip {
     z-index: 1;
     position: absolute;
+    top: -10px;
   }
   
   .tooltip .tooltip-body {
     visibility: hidden;
     width: 300px;
-    padding: 5px 5px 0 0;
+    margin-top: 5px;
     border-bottom: 2px solid #790813;
     border-left: 2px solid #790813;
     border-radius: 0 10px 0 10px;
-
+    
   /* Position the tooltip */
     position: absolute;
-    left: 15px;
+    top: 12px;
     z-index: 1;
   }
   
@@ -451,25 +426,42 @@ async function main() {
   
   .blood-potency[data] {
     left: 35px;
-    top: -12px;
   }
   
   .blood-potency[data]::after {
     content: attr(data);
     position: absolute;
-    color: white;
-    font-size: 10px;
+    color: #DCDACE;
+    font-size: 12px;
     font-weight: bold;
-    left: 5px;
-    top: 7px;
+    left: 3.2px;
+    top: 2px;
     z-index: 1;
+    pointer-events: none;
   }
-  
-  .touchstones {
-    left: -5px;
-    top: -10px;
+
+  .touchstones {}
+
+  .bi {}
+
+  .bi-tooltip:hover {
+    color:#740812;
+    cursor: pointer;
   }
-  
+
+  .bi-droplet-fill {}
+
+  .bi-people-fill {}
+
+  .bi-person-bounding-box{
+    font-size:50px;
+    position: absolute;
+    right: -25px;
+    top: -18px;
+    color:#740812;
+    background-color:#D8D6CA; 
+  }
+
   .resources-label {
     margin: 2px 0px;
     border-bottom: 2px solid #790813;
@@ -673,7 +665,7 @@ async function main() {
           transparent 100%
           );
   }
-  
+
   .body-monitor {
     overflow: hidden;
     display: flex;
@@ -693,13 +685,16 @@ async function main() {
     color: #790813;
   }   
   </style>
+  <head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <script>
+      document.getElementById('monitor-container').appendChild(div);
+    </script>
+  </head>
   <h1 class="header-label">Characters Monitor</h1>
   <body id="body" class="body-monitor">
       <div id="monitor-container" class="body-fields"></div>
   </body>
-  <script>
-      document.getElementById('monitor-container').appendChild(div);
-  </script>
   `;
   let dialog = new Dialog({
     title: "Monitor",
